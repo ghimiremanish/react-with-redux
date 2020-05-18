aef985ec66c4b3fc36d05e1056ead48a63a2b1df
Manish Ghimire <algorithm@Algorithm-2.local>
2020-05-19 02:41:35 +0545

    basic react setting

---
+++ src/container/cakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function cakeContainer() {
+    return (
+        <div style={{padding: '10%'}}>
+            <h1>Number of cakes</h1>
+            <button>BUY</button>
+        </div>
+    )
+}
+
+export default cakeContainer
